ba1a38b839aad82a58bf3f1350a5b40980270d49
Moyukh Sarkar <moyukh.sarkar@kreeti.com>
2023-03-22 15:20:49 +0530

    21 March SQLAssignment Done

---
+++ SQL_Assignments/assignment_2023_03_20.sql
@@ -0,0 +1,31 @@
+-- 1. Select all records in table “Employees” whose last name starts with ‘Sah’
+
+SELECT * FROM Employees e WHERE e.last_name LIKE 'Sah%';
+
+-- 2. Select 10 employees who were hired between 1st June, 2021 to 15th February,
+-- 2022. Start retrieving from the 4th record in the table
+
+SELECT * FROM Employees e WHERE e.hire_date >= '2021-06-01' AND e.hire_date <  '2022-02-01' LIMIT 10 OFFSET 4;
+
+-- 3. Find distinct top 5 salary values (only salary column), using alias for column as
+-- “Top 5 salaries”
+
+SELECT salary as "Top 5 salaries" FROM Employees e ORDER BY salary DESC LIMIT 5;
+
+-- 4. Find first_name, last_name and email of employees from the employees table
+-- where salary lies between 25000 to 45000
+
+SELECT first_name, last_name,manager_id, salary FROM Employees e WHERE e.salary>=25000 AND e.salary<=45000;
+
+
+-- 5. Write a query to find first_name, manager_id and salary where the
+-- department_id is 101 and sort the result in ascending order of salary.
+
+SELECT first_name,manager_id, salary FROM Employees e WHERE e.department_id=1 ORDER BY salary ASC;
+
+-- 6. Find all the first_name that start with the letter “A”, and find all the last_name
+-- that have “TH” in the 2nd and 3rd positions.
+
+SELECT first_name FROM Employees e WHERE e.first_name LIKE 'A%';
+
+SELECT last_name FROM Employees e WHERE e.last_name LIKE '_TH%';
---
+++ SQL_Assignments/assignment_2023_03_21.sql
@@ -0,0 +1,38 @@
+-- 1. Write the query to get the department and department wise total(sum) salary,
+-- display it in ascending order according to salary.
+
+SELECT department_name,SUM(salary) from employees e, departments d WHERE e.department_id=d.department_id GROUP BY department_name;
+
+
+-- 2. Write the query to get the department, total no. employee of each department,
+-- total(sum) salary with respect to department from "Employee" table.
+
+SELECT department_name,COUNT(first_name) as "Total people",SUM(salary) as "Total department Salary" FROM employees e, departments d WHERE e.department_id=d.department_id GROUP BY department_name;
+
+
+-- 3. Get department wise maximum salary from "Employee" table order by salary
+-- ascending
+
+SELECT department_name,MAX(salary) as "Maximum Salary" FROM employees e , departments d WHERE e.department_id=d.department_id GROUP BY department_name ORDER BY MAX(salary) ASC;
+
+-- 4. Write a query to get the departments where average salary is more than 60k
+
+SELECT department_name FROM employees e , departments d WHERE e.department_id=d.department_id GROUP BY department_name HAVING AVG(salary) >=60000 ;
+
+-- 5. Write down the query to fetch department name assign to more than one
+-- Employee
+
+SELECT department_name FROM employees e , departments d WHERE e.department_id=d.department_id GROUP BY department_name HAVING COUNT(department_name)>1 ;
+
+-- 6. Write a query to show department_name and assignedTo where assignedTo
+-- will be “One candidate” if its assigned to only one employee otherwise
+-- “Multiple candidates”
+
+SELECT department_name,
+CASE 
+    WHEN COUNT(department_name)=1 THEN 'One candidate'
+    WHEN COUNT(department_name)>1 THEN 'Multiple candidates'
+    END
+AS "assignedTo"
+ FROM employees e , departments d WHERE e.department_id=d.department_id GROUP BY department_name;
+
